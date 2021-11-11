3cdf11f69018d9a900ae3f2558a9daab7456824b
Frank <fengxuting@qq.com>
2021-11-11 16:40:59 +0800

    init

---
+++ src/lib/web3.ts
@@ -0,0 +1,18 @@
+import Web3 from 'web3';
+import { WebsocketProvider, HttpProvider, IpcProvider } from 'web3-core';
+
+let web3: Web3;
+let provider: WebsocketProvider | HttpProvider | IpcProvider;
+export function init(providerPath: string): Web3 {
+  provider = new Web3.providers.HttpProvider(providerPath);
+  web3 = new Web3(provider);
+  return web3;
+}
+
+export function getInstance(): Web3 {
+  return web3;
+}
+
+export function getProvider(): WebsocketProvider | HttpProvider | IpcProvider {
+  return provider;
+}
